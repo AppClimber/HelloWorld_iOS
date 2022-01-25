3ef17b374c36979cd92be304c66791101de2511d
Masahiko Minagawa <Masahiko.Minagawa@sony.com>
2022-01-25 14:31:08 +0900

    Initial Commit

---
+++ HelloWorld_iOS/HelloWorld_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorld_iOSApp.swift
+//  HelloWorld_iOS
+//
+//  Created by Masahiko Minagawa on 2022/01/25.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorld_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
